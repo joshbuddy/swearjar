fc7ef506652d2e1a1229fbf9cf2bd8baec33c757
Jeff Vyduna <web@jeffvyduna.com>
2011-04-13 18:42:27 -0700

    Added naive support for automatic pluralization

--- spec/swearjar_spec.rb
+++ spec/swearjar_spec.rb
@@ -9,7 +9,12 @@ describe Swearjar do
   it "should detect dirty words regardless of case" do
     Swearjar.default.profane?('FuCk you jim henson').should be_true
   end
-
+  
+  it "should detect simple dirty plurals" do
+    Swearjar.default.profane?('jim henson had two dicks').should be_true
+    Swearjar.default.profane?('jim henson has two asses').should be_true
+  end
+  
   it "should not detect non-dirty words" do
     Swearjar.default.profane?('i love you jim henson').should be_false
   end
@@ -20,6 +25,10 @@ describe Swearjar do
 
   it "should detect multiword" do
     Swearjar.default.scorecard('jim henson has a hard on').should == {:sexual=>1}
+  end
+  
+  it "should detect multiword plurals" do
+    Swearjar.default.scorecard('jim henson has a hard ons').should == {:sexual=>1}
   end
 
   it "should censor a string" do
