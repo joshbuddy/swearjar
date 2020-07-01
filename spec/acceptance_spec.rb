28dee113580d9f47ef9a7b4cd08427fd49948bf5
Michael Foley <michael.foley@salesforce.com>
2020-07-01 16:46:15 -0700

    Add acceptance test for a passage without swears

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 describe Swearjar do
-  def original_text
-    File.open(File.expand_path("../data/carlin_dirty_words.txt", __FILE__)).read
-  end
-
-  def expected_censored_text
-    File.open(File.expand_path("../data/carlin_dirty_words_censored.txt", __FILE__)).read
-  end
-
   it "censors George Carlin's Seven Dirty Words routine" do
+    original_text = File.open(File.expand_path("../data/carlin_dirty_words.txt", __FILE__)).read
+    expected_censored_text = File.open(File.expand_path("../data/carlin_dirty_words_censored.txt", __FILE__)).read
     censored_text = Swearjar.default.censor(original_text)
+
     expect(censored_text).to eq(expected_censored_text)
+  end
+
+  it "does not censor Epictetus' Enchiridion" do
+    original_text = File.open(File.expand_path("../data/epictetus_enchiridion.txt", __FILE__)).read
+    censored_text = Swearjar.default.censor(original_text)
+
+    expect(censored_text).to eq(original_text)
   end
 end
